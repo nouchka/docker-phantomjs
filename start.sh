021889c2d3d037e40539fe070175b46a75bc91df
nouchka <github@katagena.com>
2018-01-28 18:21:19 +0100

    Only keep result

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ for i in "${!ids[@]}"
 do
 	ID=${ids[$i]}
 	URL=${sites[$i]}
-	TIME=`/usr/local/bin/phantomjs --ssl-protocol=any /usr/local/share/loadspeed.js $URL|sed 's/[^0-9]//g'`
+	TIME=`/usr/local/bin/phantomjs --ssl-protocol=any /usr/local/share/loadspeed.js $URL|tail -n1|sed 's/[^0-9]//g'`
 	echo $ID $TIME
 done
